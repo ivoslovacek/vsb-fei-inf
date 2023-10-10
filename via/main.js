6084f459050bacf1100d27b80b7296278d980c6c
Ivo Slováček <ivoslovacek@gmail.com>
2023-10-10 09:48:07 +0200

    updated

--- via/main.js
+++ via/main.js
@@ -76,9 +76,37 @@ window.addEventListener('load', () => {
         })
     }
 
+    const todoSaveBttn = document.getElementById("todo-save");
+    todoSaveBttn.addEventListener('click', () => {
+        const todoHead = document.getElementById("todo-header");
+        const todoDesc = document.getElementById("todo-desc");
+
+        const todoObject = {
+            head: todoHead.value.toString(),
+            desc: todoDesc.value.toString(),
+        };
+
+        const todoObjectString = JSON.stringify(todoObject);
+
+        let todoIndex = localStorage.getItem("lastTodo");
+        if (todoIndex == null) {
+            todoIndex = 0;
+        }
+        else {
+            todoIndex++;
+        }
+
+        localStorage.setItem(todoIndex, todoObjectString);
+        localStorage.setItem("lastTodo", todoIndex);
+
+        todoHead.value = '';
+        todoDesc.value = '';
+    });
+
     updateTimeString();
     updateTimeStamp();
 });
+
 
 function updateTimeStamp() {
     const timeStamp = document.getElementById('timeStamp');
