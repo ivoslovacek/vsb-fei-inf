d439f890f0461a702553e842c66a2f7652e8184e
Ivo Slováček <ivoslovacek@gmail.com>
2023-11-30 16:46:39 +0100

    Added image carousel

--- via/projectb/articles/sports/F1/abu_dhabi_gp/resources/main.js
+++ via/projectb/articles/sports/F1/abu_dhabi_gp/resources/main.js
@@ -1,5 +1,9 @@
+var jsonData = null;
+var currentImage = 0;
+
 window.addEventListener("load", () => {
     loadArticleCreationTime();
+    loadImageCarousel();
 });
 
 async function loadArticleCreationTime() {
@@ -42,4 +46,67 @@ async function loadArticleCreationTime() {
     const timeElement = document.getElementById("time");
 
     timeElement.innerHTML = articleDateString;
+}
+
+async function loadImageCarousel() {
+    await fetch("resources/images.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            jsonData = data;
+        })
+        .catch(error => {
+            console.error('Error loading JSON:', error);
+        });
+
+    const leftBtn = document.getElementById("left-btn");
+    const rightBtn = document.getElementById("right-btn");
+
+    leftBtn.addEventListener('click', () => {
+        currentImage--;
+        loadCurrentImage();
+    });
+
+    rightBtn.addEventListener('click', () => {
+        currentImage++;
+        loadCurrentImage();
+    });
+
+    loadCurrentImage();
+}
+
+function loadCurrentImage() {
+    const image = jsonData.images[currentImage];
+
+
+    const path = "images/" + image.fileName;
+    const description = image.description;
+
+    const imgElement = document.getElementById("carousel-img");
+    const descriptionElement = document.getElementById("description");
+
+    imgElement.src = path;
+    descriptionElement.innerHTML = description;
+
+
+    const leftBtn = document.getElementById("left-btn");
+    const rightBtn = document.getElementById("right-btn");
+
+    if (currentImage === 0) {
+        leftBtn.style.visibility = "hidden";
+    }
+    else {
+        leftBtn.style.visibility = "visible";
+    }
+
+    if (currentImage === jsonData.images.length - 1) {
+        rightBtn.style.visibility = "hidden";
+    }
+    else {
+        rightBtn.style.visibility = "visible";
+    }
 }
