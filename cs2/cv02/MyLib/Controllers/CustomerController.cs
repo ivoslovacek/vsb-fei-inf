b3a30630900a3ec5dab9a494324bb205c4ab9e25
Ivo Slováček <ivoslovacek@gmail.com>
2024-02-27 23:25:00 +0100

    Added cs2/cv02

---
+++ cs2/cv02/MyLib/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using MyLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.Controllers
+{
+    class CustomerController
+    {
+
+        private static List<Customer> customers = new List<Customer>()
+        {
+            new Customer(){ Id = 1, Age = 34, IsActive = true, Name = "Jan" },
+            new Customer(){ Id = 2, Age = 46, IsActive = false, Name = "Tom" },
+            new Customer(){ Id = 3, Age = 36, IsActive = true, Name = "Michala" }
+        };
+
+        public string List(int limit)
+        {
+            StringBuilder result = new StringBuilder();
+            int i = 0;
+            foreach(var customer in customers)
+            {
+                if (i < limit)
+                {
+                    result.AppendLine(customer.Name);
+                }
+                else
+                {
+                    break;
+                }
+
+                i++;
+            }
+            return result.ToString();
+        }
+
+    }
+}
---
+++ cs2/cv02/ReflectionApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+
+
+
+namespace ReflectionApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Path.GetFullPath("../../../../MyLib/bin/Debug/net6.0/MyLib.dll");
+            
+            Assembly assembly = Assembly.LoadFile(path);
+            
+            Console.WriteLine(assembly.FullName);
+
+            Type[] types = assembly.GetTypes();
+
+            object obj = assembly.CreateInstance("MyLib.Controllers.CustomerController");
+
+            Type controllerType = assembly.GetType("MyLib.Controllers.CustomerController");
+            MethodInfo listMethod = controllerType.GetMethod("List");
+
+            string result = (string)listMethod.Invoke(obj, new object[] { 2 });
+            
+            Console.WriteLine(result);
+
+            string url = "/Customer/List?limit=2";
+
+            string[] parts = url.Split('?');
+            string[] left = parts[0].Split('/');
+            string[] right = parts[1].Split('&');
+
+            string controllerName = left[1];
+            string methodName = left[2];
+
+            Dictionary<string, string> arguments = right.Select(x => x.Split('=')).ToDictionary(x => x[0], y => y[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+            object customerobj = assembly.CreateInstance("Mylib.Models.Customer");
+
+
+
+        }
+    }
+}
